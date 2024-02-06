125421789dbd6347ffa3f163333b862c80049d89
Elliot Fox <1209756@sd61learn.ca>
2024-02-05 17:09:38 -0800

    Added intake rotation methods in IntakeSubsystem.

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -10,6 +10,7 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.Constants.IntakeConstants;
 
 public final class IntakeSubsystem extends SubsystemBase {
@@ -18,12 +19,17 @@ public final class IntakeSubsystem extends SubsystemBase {
     private final CANSparkMax intakeMotor;
 
     private final SparkPIDController intakeController;
+    private final SparkPIDController rotationController;
 
     private final DigitalInput intakePositionLimit;
     private final DigitalInput indexPositionLimit;
 
+    // Command to rotate until it hits one of the limit switches
+    // Command to rotate until it hits the other limit switch
+
     public IntakeSubsystem() {
         rotationMotor = new CANSparkMax(-1, MotorType.kBrushless);
+        rotationController = rotationMotor.getPIDController();
         configureRotationMotor();
 
         intakeMotor = new CANSparkMax(-1, MotorType.kBrushless);
@@ -36,7 +42,7 @@ public final class IntakeSubsystem extends SubsystemBase {
 
     private void configureIntakeMotor() {
         intakeMotor.setIdleMode(IdleMode.kBrake);
-        intakeMotor.enableVoltageCompensation(12.2);
+        intakeMotor.enableVoltageCompensation(IntakeConstants.INTAKE_MAX_VOLTAGE);
         intakeMotor.setInverted(false);
 
         intakeController.setP(IntakeConstants.INTAKE_CONTROLLER_KP);
@@ -48,7 +54,16 @@ public final class IntakeSubsystem extends SubsystemBase {
     }
 
     private void configureRotationMotor() {
+        rotationMotor.setIdleMode(IdleMode.kBrake);
+        rotationMotor.enableVoltageCompensation(Constants.IntakeConstants.ROTATION_MAX_VOLTAGE);
+        rotationMotor.setInverted(false);
 
+        rotationController.setP(IntakeConstants.ROTATION_CONTROLLER_KP);
+        rotationController.setI(IntakeConstants.ROTATION_CONTROLLER_KI);
+        rotationController.setD(IntakeConstants.ROTATION_CONTROLLER_KD);
+        rotationController.setIZone(IntakeConstants.ROTATION_CONTROLLER_IZ);
+        rotationController.setFF(IntakeConstants.ROTATION_CONTROLLER_FF);
+        rotationController.setOutputRange(-1, 1);
     }
 
     public Command intakeNote() {
@@ -59,19 +74,23 @@ public final class IntakeSubsystem extends SubsystemBase {
         return run(() -> outtake(IntakeConstants.OUTTAKE_RPM));
     }
 
+    public Command stopIntakeMotor() {
+        return run((this::stopIntaking));
+    }
 
     /** Rotates the intake until it is in index position. */
     public Command intakeToIndexPosition() {
-        return run(() -> rotateIntake(-1)).until(this::atIndexPosition).andThen(this::stopRotation);
+        return run(() -> rotateIntake(IntakeConstants.INTAKE_TO_INDEX_RPM)).until(this::atIndexPosition).andThen(this::stopRotation);
     }
 
     /** Rotates the intake until it is in intake position */
     public Command intakeToIntakePosition() {
-        return run(() -> rotateIntake(1)).until(this::atIntakePosition).andThen(this::stopRotation);
-    }
+        return run(() -> rotateIntake(IntakeConstants.INTAKE_TO_INTAKE_RPM)).until(this::atIntakePosition).andThen(this::stopRotation);
+    }                                                                                                    
 
     private void rotateIntake(double rpm) {
-
+        rpm = Math.min(rpm, IntakeConstants.ROTATION_MAX_RPM);
+        rotationController.setReference(rpm, ControlType.kVelocity);
     }
 
     private void intake(double rpm) {
@@ -85,9 +104,7 @@ public final class IntakeSubsystem extends SubsystemBase {
     }
 
     private void stopRotation() { rotationMotor.set(0); }
-    private void stopIntaking() {
-        intakeMotor.set(0);
-    }
+    private void stopIntaking() { intakeMotor.set(0); }
     
 
     private boolean atIntakePosition() { return intakePositionLimit.get(); }
